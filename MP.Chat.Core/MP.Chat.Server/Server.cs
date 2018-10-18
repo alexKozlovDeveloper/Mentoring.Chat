71a9dc758c559b4e0d39e251120ef5a1f5a82bb9
alexkozlovdeveloper <alexkozlovdeveloper@gmail.com>
2018-10-18 16:41:40 +0300

    Add details log to server

--- MP.Chat.Core/MP.Chat.Server/Server.cs
+++ MP.Chat.Core/MP.Chat.Server/Server.cs
@@ -24,40 +24,45 @@ namespace MP.Chat.Server
             _logger = logger;
 
             _messageStore = new MessageStore(_logger);
-            _clientHandlers = new List<ClientHandler>();            
+            _clientHandlers = new List<ClientHandler>();
         }
 
         public void StartListening()
         {
-            _logger.Info("Creating listening pipe.");
+            _logger.Info("Creating listening pipe...");
             NamedPipeServerStream pipeServer = new NamedPipeServerStream(Constant.ServerListeningPipeName, PipeDirection.InOut, 1);
 
             while (true)
             {
-                // Wait for a client to connect
-                Console.WriteLine("Waiting For Connection.");
+                _logger.Info("Waiting for new connection...");
                 pipeServer.WaitForConnection();
 
-                Console.WriteLine("Client connected");
+                _logger.Info("New client connected");
 
                 try
                 {
-                    StreamString ss = new StreamString(pipeServer);
+                    StreamController streamController = new StreamController(pipeServer);
 
-                    Console.WriteLine("Reading command");
-                    //ss.WriteString("I am the one true server!");
-                    var chatMessageStr = ss.ReadString();
-                    var chatMessage =  JsonConvert.DeserializeObject<ChatMessage>(chatMessageStr);
+                    _logger.Info("Reading message from user...");
+                    var chatMessage = streamController.ReceiveMessage();
 
-                    if(chatMessage.Command == ChatCommand.RegisterUser)
+                    _logger.Info($"User name '{chatMessage.Name}'");
+
+                    if (chatMessage.Command == ChatCommand.RegisterUser)
                     {
-                        Console.WriteLine("RegisterUser command");
+                        _logger.Info($"[{chatMessage.Name}] 'RegisterUser' command received");
+                        _logger.Info($"[{chatMessage.Name}] Creating user id...");
                         var id = RandomHelper.Random.Next(0, 1000).ToString();
 
-                        Console.WriteLine($"Id '{id}'");
+                        _logger.Info($"[{chatMessage.Name}] User Id '{id}'");
+                        _logger.Info($"[{chatMessage.Name}|{id}] Creating client handler...");
 
-                        var clientHandler = new ClientHandler(_logger, id, chatMessage.Content, _messageStore);
+                        var clientHandler = new ClientHandler(_logger, id, chatMessage.Name, _messageStore);
+
+                        _logger.Info($"[{chatMessage.Name}|{id}] Starting client handler...");
                         clientHandler.Start();
+
+                        _logger.Info($"[{chatMessage.Name}|{id}] Sending approve message to client with user id...");
 
                         var infoMessageToClient = new ChatMessage()
                         {
@@ -65,21 +70,29 @@ namespace MP.Chat.Server
                             Content = id
                         };
 
-                        Console.WriteLine("Sending id to client");
+                        streamController.SendMessage(infoMessageToClient);
+                        _logger.Info($"[{chatMessage.Name}|{id}] Disconnecting...");
 
-                        var infoMessageToClientstr = JsonConvert.SerializeObject(infoMessageToClient);
-                        ss.WriteString(infoMessageToClientstr);
                         pipeServer.Disconnect();
-                        Console.WriteLine("Disconnected");
                     }
 
-                    // Read in the contents of the file while impersonating the client.
-                    //ReadFileToStream fileReader = new ReadFileToStream(ss, filename);
+                    if (chatMessage.Command == ChatCommand.GetMessagesStory)
+                    {
+                        _logger.Info($"[{chatMessage.Name}] 'GetMessagesStory' command received");
 
-                    //// Display the name of the user we are impersonating.
-                    //Console.WriteLine("Reading file: {0} on thread[{1}] as user: {2}.",
-                    //    filename, threadId, pipeServer.GetImpersonationUserName());
-                    //pipeServer.RunAsClient(fileReader.Start);
+                        _logger.Info($"[{chatMessage.Name}] Sending messages history to client...");
+
+                        var infoMessageToClient = new ChatMessage()
+                        {
+                            Command = ChatCommand.GetMessagesStory,
+                            Content = JsonConvert.SerializeObject(_messageStore.Messages)
+                        };
+
+                        streamController.SendMessage(infoMessageToClient);
+                        _logger.Info($"[{chatMessage.Name}] Disconnecting...");
+
+                        pipeServer.Disconnect();
+                    }
                 }
                 // Catch the IOException that is raised if the pipe is broken
                 // or disconnected.
@@ -87,10 +100,10 @@ namespace MP.Chat.Server
                 {
                     Console.WriteLine("ERROR: {0}", e.Message);
                 }
-               // Console.WriteLine("Closing...");
+                // Console.WriteLine("Closing...");
                 //pipeServer.Close();
-                
-            }            
+
+            }
         }
 
         public void ClientThreadFunc()
